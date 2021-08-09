17c74eadd74707667c11e82a3c59bd3661fa3238
Armando Gonzalez <armangonzal@gmail.com>
2021-08-09 14:11:16 -0500

    Add name and info output

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main (String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("This is a commit.");
+        System.out.println("Armando Gonzalez");
+        System.out.println("833 E Sunnyside Ave");
+        System.out.println("989-999-9926");
     }
 }
