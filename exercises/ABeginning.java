26b55bf2981f8f5d64106fdc8431515a172f63df
Armando Gonzalez <armangonzal@gmail.com>
2021-08-10 14:35:37 -0500

    ABeginning exercise

---
+++ exercises/ABeginning.java
@@ -0,0 +1,18 @@
+public class ABeginning {
+    public static void main(String[] args) {
+        String[] phrases = {
+                "Hello World!",
+                "Hello from the Guild!",
+                "Typing code is easier than I thought ...",
+                "Typity Typity Type!",
+                "After I finish typing,",
+                "I'll compile my code.",
+                "And then when I run it,",
+                "The console will print out all my brilliant words!",
+                "And it all starts with 'Hello World!' .."
+        };
+
+        for(String phrase: phrases)
+            System.out.println(phrase);
+    }
+}
