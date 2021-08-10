32be718f0df128f34ee82ba79b19b1504083d26c
Armando Gonzalez <armangonzal@gmail.com>
2021-08-10 14:44:01 -0500

    ProjectGutenberg exercise update

--- exercises/ProjectGutenberg.java
+++ exercises/ProjectGutenberg.java
@@ -1,10 +1,10 @@
 public class ProjectGutenberg {
     public static void main(String[] args) {
         String[] phrases = {
-                "Did you know that in 1440 (or thereabouts),\n",
+                "Did you know that in 1440 (or thereabouts),",
                 "Johannes Gutenberg invented the printing press?\n",
                 "He started out as a goldsmith!\n",
-                "His invention made it easy to print and\n",
+                "His invention made it easy to print and",
                 "distribute books to anyone who wanted one.\n",
                 "We are like a modern Gutenberg,\n",
                 "printing vast amounts to the waiting console with ease.\n",
