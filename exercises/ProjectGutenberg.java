44cab881e7ec1eed2a8f192017e7a9e6c0025635
Armando Gonzalez <armangonzal@gmail.com>
2021-08-10 14:42:27 -0500

    ProjectGutenberg exercise

---
+++ exercises/ProjectGutenberg.java
@@ -0,0 +1,16 @@
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+        String[] phrases = {
+                "Did you know that in 1440 (or thereabouts),\n",
+                "Johannes Gutenberg invented the printing press?\n",
+                "He started out as a goldsmith!\n",
+                "His invention made it easy to print and\n",
+                "distribute books to anyone who wanted one.\n",
+                "We are like a modern Gutenberg,\n",
+                "printing vast amounts to the waiting console with ease.\n",
+        };
+
+        for(String phrase: phrases)
+            System.out.print(phrase);
+    }
+}
